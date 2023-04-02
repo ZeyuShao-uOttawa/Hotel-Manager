37c8e82108652ebfb47d508c6c089b24b120c0e8
Eric Liu <eeric048@uottawa.ca>
2023-04-02 19:37:57 -0400

    created a .sql file where all the important queries will be located

---
+++ server/queries.sql
@@ -0,0 +1,22 @@
+-- searching by city
+SELECT * FROM hotel WHERE address LIKE '%Montreal%' OR address LIKE '%Ottawa%';
+
+
+-- important details about ONLY hotel chains
+SELECT HC_id, address, email, phone FROM hotel_chain
+
+-- important details about ONLY hotel
+SELECT hc_id, hotel_id, rating, address, email, phone FROM hotel
+
+-- surface details of room
+SELECT price, capacity, room.hotel_id, hotel.address
+FROM room 
+INNER JOIN hotel ON hotel.hotel_id = room.hotel_id
+WHERE (NOT is_rented);
+
+
+
+-- join hotel chain id, HC address, hotel id, hotel address
+SELECT  hotel_chain.HC_id, hotel_chain.address, hotel.hotel_id, hotel.address
+FROM hotel
+INNER JOIN hotel_chain ON hotel.HC_id = hotel_chain.HC_id;
