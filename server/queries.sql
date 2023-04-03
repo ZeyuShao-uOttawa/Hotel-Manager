f9e9608cef7b0ee5fef6f0f23b112ae918499aa9
Eric Liu <eeric048@uottawa.ca>
2023-04-03 15:08:08 -0400

    added some more queries

--- server/queries.sql
+++ server/queries.sql
@@ -18,3 +18,31 @@ WHERE (NOT is_rented);
 SELECT  hotel_chain.HC_id, hotel_chain.address, hotel.hotel_id, hotel.address
 FROM hotel
 INNER JOIN hotel_chain ON hotel.HC_id = hotel_chain.HC_id;
+
+-- retrieve all rooms that can be extended
+SELECT room_id
+FROM room
+WHERE can_be_extended;
+
+-- Retrieve all rooms and their booking history
+SELECT room.room_id, booking.date, customer.name
+FROM room
+LEFT JOIN booking ON room.room_id = booking.room_id
+LEFT JOIN customer ON booking.SSN = customer.SSN;
+
+--customer can view their bookings
+SELECT booking.booking_id, customer.name
+FROM booking
+LEFT JOIN customer ON booking.SSN = customer.SSN;
+
+--retrieve all rooms and their amenities
+SELECT room.room_id, amenity.amenity_name
+FROM room
+INNER JOIN room_amenity ON room.room_id = room_amenity.room_id
+INNER JOIN amenity ON room_amenity.amenity_id = amenity.amenity_id;
+
+--Retrieve all customers who have rented a room
+SELECT customer.name, renting.renting_id
+FROM customer
+INNER JOIN renting ON customer.SSN = renting.SSN;
+
